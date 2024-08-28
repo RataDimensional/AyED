75942c69b75e36f91765372b64967bb002328870
RataDimensional <mmirandacosme@frba.utn.edu.ar>
2024-08-27 23:28:24 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+    cout << "Hola mundo" << endl;  // Se imprime "Hola mundo" y se dejael cursor en la siguiente línea
+    return 0;  // Indica que el programa se ejecuto correctamente
+}
